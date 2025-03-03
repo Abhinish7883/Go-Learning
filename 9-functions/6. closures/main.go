c7d93ac986de160a13f9729abb40c26df25e4bfc
Abhinish Tiwari <abhinish990@gmail.com>
2025-03-03 22:50:55 +0530

    learn closures..

--- 9-functions/6. closures/main.go
+++ 9-functions/6. closures/main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+func counter() func() int {
+	count := 0
+	return func() int {
+		count++
+		return count
+	}
+}
+
+func rateLimiter(maxCalls int, duration time.Duration) func() bool {
+	count := 0
+	lastReset := time.Now()
+
+	return func() bool {
+		if time.Since(lastReset) > duration {
+			count = 0
+			lastReset = time.Now()
+		}
+		if count < maxCalls {
+			count++
+			return true
+		}
+		return false
+	}
+}
+
 func main() {
-	// Need to implement concepts.
+	increment := counter()
+	fmt.Println(increment())
+	fmt.Println(increment())
+
+	// Usage: Allow 3 requests/second
+	checkLimit := rateLimiter(3, time.Second)
+	fmt.Println(checkLimit())
+
 }
