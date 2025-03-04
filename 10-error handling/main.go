223a1c71907d09d28697489c68869c9ce01a65cc
Abhinish Tiwari <abhinish990@gmail.com>
2025-03-04 23:21:28 +0530

    start error handling..

--- 10-error handling/main.go
+++ 10-error handling/main.go
@@ -12,14 +12,47 @@ func divide(a int, b int) (int, error) {
 	return a / b, nil
 }
 
+func customErrorCheck(id int) (string, error) {
+	if id <= 0 {
+		return "", fmt.Errorf("invalid user ID: %d", id)
+	}
+	return "User 123", nil
+}
+
+var ErrNotFound = errors.New("item not found")
+
+func findItem(id int) error {
+	if id != 1 {
+		return fmt.Errorf("findItem: %w", ErrNotFound) // Wrapping error
+	}
+	return nil
+}
+
 func main() {
 
 	number, err := divide(3, 1)
 
+	// error Check
 	if err != nil {
 		fmt.Println("Error: ", err)
 	} else {
 		fmt.Println("Result: ", number)
+	}
+
+	// custom error check
+	user, err := customErrorCheck(-1)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("User :", user)
+	}
+
+	// Using errors.Is() and errors.As() for Error Wrapping
+	error1 := findItem(2)
+	if errors.Is(error1, ErrNotFound) { // Checking error type
+		fmt.Println("Custom Error: Item not found")
+	} else if error1 != nil {
+		fmt.Println("Error:", error1)
 	}
 
 }
